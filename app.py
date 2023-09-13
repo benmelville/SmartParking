150400106e8c43e132bcf2028e278539a064364b
Benjamin <bmelville8@gatech.edu>
2023-09-13 16:37:27 -0400

    Added HTML files, Bootstrap, and simple login page. Still need to add login functionality.

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return render_template("index.html", content=["ben", "joe", "bill"])
+
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        user = request.form["name"]
+        return redirect(url_for("user", user=user))
+    return render_template("login.html")
+
+
+@app.route('/<user>')
+def user(user):
+    return f"<h1>{user}</h1>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
